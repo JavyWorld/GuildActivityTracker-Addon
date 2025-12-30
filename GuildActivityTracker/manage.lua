012a87abd7ea7e2d067f873a04601b4407b6a08c
JavyWorld <javyrivero1999@gmail.com>
2025-12-30 17:28:34 -0500

    Delete GuildActivityTracker directory

--- GuildActivityTracker/manage.lua
+++
@@ -1,6 +0,0 @@
--- manage.lua (Universal stub)
--- Este archivo existe solo para mantener compatibilidad con el .toc.
--- Funciones de gestión avanzadas (si existen) deberían vivir en el Master build.
-local addonName, GAT = ...
-
--- Si algún día quieres lógica aquí para TODOS, este stub es el lugar seguro.
